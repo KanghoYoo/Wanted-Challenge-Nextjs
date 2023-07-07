a78d96302a955fa8a0fe9a81b56ae7002a381ac6
KanghoYoo <kangsick98@gmail.com>
2023-07-07 17:31:43 +0900

    modify : 중복된 코드 제거

--- src/hooks/useRouter.tsx
+++ src/hooks/useRouter.tsx
@@ -1,7 +1,6 @@
 const useRouter = () => {
   const push = (path: string | URL | null) => {
-    history.pushState({}, "", path);
-    window.onpopstate?.(new PopStateEvent("popstate"));
+    window.history.pushState({}, "", path);
     window.dispatchEvent(new Event("popstate"));
   };
   return { push };
