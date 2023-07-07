11c157fbfff2de96b60f658add6c73dabf9258b3
KanghoYoo <kangsick98@gmail.com>
2023-07-07 16:55:28 +0900

    add : router 컴포넌트 추가

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { createContext, useEffect, useState } from "react";
 
-const Router = ({ children }: any) => {
-  return <div>{children}</div>;
+interface RouterContextType {
+  pathName: string | URL;
+}
+
+interface RouterProps {
+  children: React.ReactNode;
+}
+
+export const RouterContext = createContext<RouterContextType | null>(null);
+
+const Router = ({ children }: RouterProps) => {
+  const [pathName, setPathName] = useState<string>(window.location.pathname);
+
+  useEffect(() => {
+    setPathName(window.location.pathname);
+
+    window.addEventListener("popstate", (e: PopStateEvent) => {
+      setPathName(window.location.pathname);
+    });
+  }, []);
+
+  return (
+    <RouterContext.Provider value={{ pathName }}>
+      {children}
+    </RouterContext.Provider>
+  );
 };
 
 export default Router;
