cf113438e14e118d5563e71a56fba5a32354ca80
KanghoYoo <kangsick98@gmail.com>
2023-07-07 16:56:10 +0900

    add : useRouter 커스텀 훅 추가

--- src/hooks/useRouter.tsx
+++ src/hooks/useRouter.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const useRouter = () => {
-  return <div>useRouter</div>;
+  const push = (path: string | URL | null) => {
+    history.pushState({}, "", path);
+    window.onpopstate?.(new PopStateEvent("popstate"));
+    window.dispatchEvent(new Event("popstate"));
+  };
+  return { push };
 };
 
 export default useRouter;
