28e828bc6dff5a87d8bd7639488a6c831e3cb55c
KanghoYoo <kangsick98@gmail.com>
2023-07-07 16:59:15 +0900

    add : about 페이지 작성 및 추가

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,14 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from "react";
+import Button from "../components/Button";
 
 const About = () => {
-  return <div>about</div>;
+  return (
+    <>
+      <h1>about</h1>
+      <Button path="/">go back</Button>
+    </>
+  );
 };
 
 export default About;
