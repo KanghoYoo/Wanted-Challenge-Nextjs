f185473ac9f941ea07806a585cfb666f6948471a
KanghoYoo <kangsick98@gmail.com>
2023-07-07 16:58:23 +0900

    add : 버튼 컴포넌트로 분리 및 페이지 이동 기능 추가

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import useRouter from "../hooks/useRouter";
+
+interface ButtonProps {
+  children: string;
+  path: string;
+}
+
+const Button = ({ children, path }: ButtonProps) => {
+  const { push } = useRouter();
+  const handleButtonClick = () => {
+    push(`${path}`);
+  };
+
+  return (
+    <button type="button" onClick={() => handleButtonClick()}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
